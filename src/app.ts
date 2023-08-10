d6766de62fbee9e3cb97782b3b6f6cd3f6650e6d
Roberto Nicolau <roberto.leonardo.2000@gmail.com>
2023-08-10 09:08:47 -0300

    Adicionando CORS Allow ALL Origns

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import fastify from 'fastify'
+import cors from '@fastify/cors'
 import { answerRoutes } from './routes/answer'
 
 export const app = fastify()
-
 app.register(answerRoutes)
+ app.register(cors, { 
+  origin: '*',
+})
